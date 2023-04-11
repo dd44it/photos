a39c07b7489d958b650a72800746d5bc1d9db835
dd44it <12nelson13@gmail.com>
2023-04-11 09:38:32 +0300

    feat: add LoadNewPhotos class

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 import ExpandText from "./utils/ExpandText.js";
+import LoadNewPhotos from "./utils/LoadNewPhotos.js";
 
 document.addEventListener("DOMContentLoaded", load);
 
 function load() {
   const cardCollection = new ExpandText(".card-description");
   cardCollection.checkText();
+
+  const loadPhotos = new LoadNewPhotos(".footer", ".cards");
+  loadPhotos.begin();
+
 }
---
+++ src/utils/LoadNewPhotos.js
@@ -0,0 +1,53 @@
+import ExpandText from "./ExpandText.js";
+
+class LoadNewPhotos {
+  constructor(touchSelector, parentSelector) {
+    this.touchSelector = touchSelector;
+    this.parentSelector = document.querySelector(parentSelector);
+  }
+
+  begin() {
+    const touchElem = document.querySelector(this.touchSelector); 
+    const observer = new IntersectionObserver((entries, observer) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          this.loadPhotos();
+        }
+      }
+    });
+    observer.observe(touchElem);
+  }
+
+  loadPhotos() {
+    const url = "https://picsum.photos/v2/list?page=1&limit=9";
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => this.renderNewCards(data, this.parentSelector))
+      .catch((err) => console.log(err));
+  }
+
+  renderNewCards(data, parentWrapper) {
+    if (data && !data.length || !data) return;
+    data.forEach((item) => {
+      const cardTemplate = `
+      <div class="col-5 card photo-card p-0" style="width: 22rem;">
+        <img src=${item.download_url} class="card-img-top" alt="">
+        <div class="card-body px-4">
+            <h3 class="card-title fw-bold">Heading</h3>
+            <p class="card-description">And here full text doesn’t fit, and at the very end of it we should show a Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, similique quasi nam praesentium voluptas, animi quidem consequatur eveniet nobis vel magni veniam natus et nulla eligendi labore ducimus rerum soluta!</p>
+        </div>
+        <div class="card-footer p-3 bg-white">
+            <button class="btn fw-bold btn-primary me-3">Save to collection</button>
+            <button class="btn fw-bold btn-secondary">Share</button>
+        </div>
+      </div>`;
+  
+      parentWrapper.insertAdjacentHTML("beforeend", cardTemplate);
+      const cardCollection = new ExpandText(".card-description");
+      cardCollection.checkText();
+    });
+  }
+
+}
+
+export default LoadNewPhotos;
